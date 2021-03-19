66a2bca5e778d4d7bcffe0ae3c1683d97d0bcac2
bruno <bruno.cami@hotmail.com>
2021-03-19 14:34:24 -0300

    footer change

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example to build on the card title and make up the bulk of the card's content.</p>
                     <a className="card-link">Card link</a>
                     <a className="card-link">Another link</a>
                 </div>
